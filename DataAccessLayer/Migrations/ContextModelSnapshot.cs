c2800ec972b2e58f6907da1c3f76b6f91ceb0bfc
burcuustael <burcustael@gmail.com>
2023-04-16 22:21:20 +0300

    Admin Layout eklendi.

--- DataAccessLayer/Migrations/ContextModelSnapshot.cs
+++ DataAccessLayer/Migrations/ContextModelSnapshot.cs
@@ -297,6 +297,10 @@ namespace DataAccessLayer.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
                     b.HasKey("TestimonialId");
 
                     b.ToTable("Testimonials");
--- EntityLayer/Concrete/Testimonial.cs
+++ EntityLayer/Concrete/Testimonial.cs
@@ -19,6 +19,7 @@ namespace EntityLayer.Concrete
         public string Comment { get; set; }
 
         public string ImageUrl { get; set; }
+        public string Title { get; set; }
 
     }
 }
