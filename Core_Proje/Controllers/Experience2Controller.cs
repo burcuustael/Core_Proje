87e6c4459d1c98693673cf129005189675dfa897
burcuustael <burcustael@gmail.com>
2023-05-11 21:59:09 +0300

    AJAX işlemleri tamamlandı.

---
+++ Core_Proje/Controllers/Experience2Controller.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core_Proje.Controllers
+{
+    public class Experience2Controller : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+               
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
+            return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience b)
+        {
+            experienceManager.TAdd(b);
+            var values = JsonConvert.SerializeObject(b);
+            return Json(values);
+        }
+
+        public IActionResult GetById(int Id)
+        {
+            var v = experienceManager.TGetByID(Id);
+            var values = JsonConvert.SerializeObject(v);
+            return Json(values);
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = experienceManager.TGetByID(id);
+            experienceManager.TDelete(values);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Core_Proje/Controllers/WriterUserController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core_Proje.Controllers
+{
+    public class WriterUserController : Controller
+    {
+        WriterUserManager userManager = new WriterUserManager(new EfWriterUserDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListUser()
+        {
+            var values = JsonConvert.SerializeObject(userManager.TGetList());
+            return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddUser(WriterUser b)
+        {
+            userManager.TAdd(b);
+            var values = JsonConvert.SerializeObject(b);
+            return Json(values);
+        }
+
+    }
+}
